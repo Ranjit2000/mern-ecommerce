d6155b9423cb936274397256245a7e086ac5cc8c
Ranjit2000 <r2anjit2000@gmail.com>
2026-05-30 20:24:35 +0530

    Add Product model

---
+++ server/models/Product.js
@@ -0,0 +1,14 @@
+import mongoose from 'mongoose';
+
+const productSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  description: { type: String, required: true },
+  price: { type: Number, required: true },
+  images: [{ type: String }],
+  category: { type: String, required: true },
+  stock: { type: Number, required: true, default: 0 },
+  rating: { type: Number, default: 0 },
+  numReviews: { type: Number, default: 0 },
+}, { timestamps: true });
+
+export default mongoose.model('Product', productSchema);

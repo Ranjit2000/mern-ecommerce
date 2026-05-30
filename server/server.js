c46bf85842ec386e7ea6442636c391fcf765db23
Ranjit2000 <r2anjit2000@gmail.com>
2026-05-30 21:25:07 +0530

    Add auth controller and routes - register and login working

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ import express from 'express';
 import dotenv from 'dotenv';
 import cors from 'cors';
 import connectDB from './config/db.js';
+import authRoutes from './routes/authRoutes.js';
 
 dotenv.config();
 connectDB(); 
@@ -9,6 +10,7 @@ connectDB();
 const app = express();
 app.use(cors());
 app.use(express.json());
+app.use('/api/auth', authRoutes);
 
 app.get('/', (req, res) => {
   res.send('API is running...');
